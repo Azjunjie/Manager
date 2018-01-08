7c8b783795f06ed756888d730ad18e5037d70b17
zhangjunjie <63289624@qq.com>
2018-01-08 21:15:35 +0800

    更改颜色值

--- app/src/main/java/com/aitewei/manager/activity/ship/ShipCabinTotal2Activity.java
+++ app/src/main/java/com/aitewei/manager/activity/ship/ShipCabinTotal2Activity.java
@@ -1,8 +1,5 @@
 package com.aitewei.manager.activity.ship;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
 import com.aitewei.manager.R;
 import com.aitewei.manager.base.BaseActivity;
 import com.aitewei.manager.utils.ToolBarUtil;
